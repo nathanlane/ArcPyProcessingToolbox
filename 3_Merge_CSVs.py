a742309627472857c8a9fee65aaf78408bddd5f9
Nathan Lane <nathaniel.x.lane@gmail.com>
2019-06-27 16:44:27 +1000

    April updates - refactored for memory issues
    Refined further to deal with March running errors, and errors for combining CSVs.
    
    Instead of writing each file in memory and then combining them, the updated script now reads only 1 file into memory at a time, appends it to your defined output file and repeats.
    
    This way possibility of getting a memory error is ruled out
    
    The major drawback of this approach, though, is; as the defined output file keeps getting built up, it takes more and more time to append each later csv to it.
    
    Ex: it took around 9 hours to merge only 288 files on my end.

--- 3_Merge_CSVs.py
+++ 3_Merge_CSVs.py
@@ -6,27 +6,43 @@ path = arcpy.GetParameterAsText(0)   # Input Folder
 ptcsv = arcpy.GetParameterAsText(1)  # ptcsv for Point CSV
 pgcsv = arcpy.GetParameterAsText(2)  # ptcsv for Polygon CSV
 
+if not ptcsv.endswith('.csv') and ptcsv:
+    ptcsv = ptcsv + ".csv"
+    
+if not pgcsv.endswith('.csv') and pgcsv:
+    pgcsv = pgcsv + ".csv"
+
+
 ptfiles=[]                           # Empty list that will contain all point CSVs
 pgfiles=[]                           # Empty list that will contain all polygon CSVs
 
+
 # Prepare list of all CSV files in their corresponding variables
 for root, dirs, files in os.walk(path):
-    for file in files:
-        if file.endswith("_pt.csv"):
-             ptfiles.append(os.path.join(root, file))
-        if file.endswith("_pg.csv"):
-             pgfiles.append(os.path.join(root, file))
+    for mfile in files:
+        if mfile.endswith("_pt.csv"):
+             ptfiles.append(os.path.join(root, mfile))
+        if mfile.endswith("_pg.csv"):
+             pgfiles.append(os.path.join(root, mfile))
+
+def mergecsvs(clist, fout):
+    for c in clist:
+        cdataframe = pandas.read_csv( c )
+        cdf = cdataframe.drop('OID', axis =1)
+        if not os.path.exists(fout):
+            arcpy.AddMessage('Appending ' + os.path.split(c)[1])
+            cdf.to_csv(fout, index=False)
+        else:
+            arcpy.AddMessage('Appending ' + os.path.split(c)[1])
+            fdf = pandas.read_csv(fout)
+            cname = list(cdf.columns.values)[0]
+            fdf[cname] = cdf[cname]
+            fdf.to_csv(fout, index=False)
 
 # Merge point CSVs if present
 if (ptcsv and ptfiles):
-    ptdataframes = [ pandas.read_csv( f ) for f in ptfiles ] # add arguments as necessary to the read_csv method
-    ptdfs = [df.drop('OID', axis =1) for df in ptdataframes]
-    ptmerged = reduce(lambda left,right: pandas.merge(left,right,on='COMM_ID', how='outer'), ptdfs)
-    ptresult = ptmerged.to_csv(os.path.join(path,ptcsv))
-
+    mergecsvs(ptfiles, ptcsv)
+    
 # Merge polygon CSVs if present
 if (pgcsv and pgfiles):
-    pgdataframes = [ pandas.read_csv( f ) for f in pgfiles ] # add arguments as necessary to the read_csv method
-    pgdfs = [df.drop('OID', axis =1) for df in pgdataframes]
-    pgmerged = reduce(lambda left,right: pandas.merge(left,right,on='NUTS_ID1', how='outer'), pgdfs)
-    pgresult = pgmerged.to_csv(os.path.join(path,pgcsv))
+    mergecsvs(pgfiles, pgcsv)   
