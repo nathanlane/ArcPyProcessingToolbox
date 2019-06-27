575424ba0c6ff41d669efae35c04a2ffce9e11b9
Nathan Lane <nathaniel.x.lane@gmail.com>
2019-06-27 16:34:47 +1000

    Late February refactoring
    This is 24219.zip, changed from 020219-1.zip. Overhaul and further refactoring.
    
    "1. The polygon shapefile you provided contain administrative boundaries at 4 different levels merged into 1 shapefile causing repetitive overlaps at each geographic location.
    
    Solution:- I tried different solutions to overcome this issue. I first split the shapefile such that each individual feature was converted into an isolated shapefile thus producing 1920 shapefiles in total.
    The process was taking so much time (3+ hours for splitting only) that made this solution impractical.
    I then tried to convert each individual feature into a separate layer file. That also took a lot of time (around 45 min for shp to lyrs conversion only), hence rendered this solution useless too.
    Finally, a thorough inspection of the shapefile revealed that each admin level is already classified by a field "STAT_LEVL_" . I then used this field to split the original shapefile into only 4 shapefiles (1 for each different admin level). This process takes considerably less time (less than a min) making it practical approach."

--- 2_Calculate_Statistics.py
+++ 2_Calculate_Statistics.py
@@ -12,6 +12,7 @@ ptvf = arcpy.GetParameterAsText(2)      # Point Value Field
 ptinter = arcpy.GetParameter(3)         # Interpolate values at point locations
 pgshp = arcpy.GetParameterAsText(4)     # Input Polygon Shapefile
 pgvf = arcpy.GetParameterAsText(5)      # Polygon Value Field
+pgsf = arcpy.GetParameterAsText(6)      # Polygon Split Field
 
 # Define local variables for calculating statistics
 if ptinter == True:
@@ -31,9 +32,42 @@ if pgshp:
     if not pgvf:
         arcpy.AddMessage("ERROR: Please select a unique values field from Polygon shapefile")
         arcpy.AddMessage("ERROR: Process will now terminate")
-        quit()
+        quit()   
 
-# Define variables
+    # List of all shapefiles in the workspace
+    shps = arcpy.ListFiles("*.shp")
+
+    # If splitted shapefiles not already exist
+    if not shps:
+
+        # Start procedure to split input shapefiles
+        fgdb = root + os.sep + "fGDB.gdb"
+        fcname = os.path.split(pgshp)[1]
+        fcname = fcname.replace('.shp', '')
+        infc = fgdb + os.sep + fcname
+
+        # Create File GDB
+        if not arcpy.Exists(fgdb):
+            arcpy.CreateFileGDB_management(root, "fGDB.gdb")
+
+        # Convert input polygon shp to feature class
+        arcpy.FeatureClassToGeodatabase_conversion(pgshp, fgdb)
+
+        # Split shapefile by unique field into layers
+        arcpy.AddMessage("Splitting polygon shapefile")
+        arcpy.SplitByAttributes_analysis(infc, root, pgsf)
+
+        # Delete file Geodatabase
+        if arcpy.Exists(fgdb):
+            arcpy.Delete_management(fgdb)
+
+# Set Workspace to input folder
+arcpy.env.workspace = root
+
+# Enable overwriting
+arcpy.env.overwriteOutput = True
+
+# Define variables related to tiff files in the input folder
 pattern = "prate_*.tif"                 # Pattern that will be used to find & prepare a list of raster files
 lTIFs = []                              # Create a blank list that would be populated by input geotiff files later
 
@@ -59,6 +93,19 @@ for tif in lTIFs:
     tbloutfield = ptcsv.split('.')[0]           # Get output csv filname without extension
     tbloutfield = tbloutfield.replace('_pt', '')# Strip _pt from name
     tbloutfield = "d"+tbloutfield[1:]           # Replace first char of date(year) by "d" to overrule a restriction
+
+    #####################################################################
+    ## Definition of variables related to Polygon Shapefile Processing ##
+    #####################################################################
+
+    pgcsv = ptcsv.replace('_pt.csv', '_pg.csv')
+    pgdbf = ptcsv.replace('_pt.csv', '_pg.dbf')
+    pgcsvp = os.path.join(tifpath, pgcsv)
+    inTables = []
+
+    ##############################################################
+    ## Start process to calulate statistics for point shapefile ##
+    ##############################################################
     
     if ptshp:
 
@@ -93,9 +140,53 @@ for tif in lTIFs:
             arcpy.Delete_management(ptout)            
         else:
             arcpy.AddMessage('Output already exists. Skipping ' + tifname)
-        del ptcsv, tbloutfield
+        del ptcsv
         
+    ################################################################
+    ## Start process to calulate statistics for polygon shapefile ##
+    ################################################################
+        
+    if pgshp:
 
+        arcpy.AddMessage('Processing ' + tifname)
+        try:
+            arcpy.Delete_management("tempras")
+        except:
+            continue
+        arcpy.Resample_management(tif, "tempras", "0.04 0.04", "NEAREST")
+        # list all fcs in workspace
+        fcs = arcpy.ListFiles("*.shp")
+        for fc in fcs:           
+      
+            sfcname = fc.replace('.shp', '')        # Splited shapefile name without extension
+            
+            pgtmpdbf = fc.replace('.shp', '_pg_')   # Prepare temporary table name that'll contain mean values
+            pgtmpdbf = pgtmpdbf + sfcname + '.dbf'  # Finalize temporary table name
+            arcpy.AddMessage('Calculating Polygon statistics for ' + sfcname)
+
+            ZonalStatisticsAsTable(fc, pgvf, "tempras", pgtmpdbf, "DATA", "MEAN")
+            arcpy.AddField_management(pgtmpdbf, "NUTS_ID1", "TEXT", field_length="5")
+            arcpy.CalculateField_management(pgtmpdbf, "NUTS_ID1", '!NUTS_ID!', "PYTHON_9.3")
+            arcpy.AddField_management(pgtmpdbf, tbloutfield, "DOUBLE", "", "", "", "", "NULLABLE")
+            arcpy.CalculateField_management(pgtmpdbf, tbloutfield, '!MEAN!', "PYTHON_9.3")
+            pgfieldList = arcpy.ListFields(pgtmpdbf)  #get a list of temp point shp fields 
+            for pgfield in pgfieldList: #loop through each field                
+                if not (pgfield.name == "OID" or pgfield.name == "NUTS_ID1" or pgfield.name == tbloutfield):
+                    try:
+                        arcpy.DeleteField_management(pgtmpdbf, pgfield.name)
+                    except:
+                        arcpy.AddMessage("Error Deleting Field " + pgfield.name)
+
+            inTables.append(pgtmpdbf)
+
+        arcpy.AddMessage('Merging temp tables into ' + pgdbf)
+        arcpy.Merge_management(inTables,pgdbf)
+        arcpy.TableToTable_conversion(pgdbf, root, pgcsv)
+        arcpy.Delete_management(pgdbf)
+        arcpy.Delete_management("tempras")
+        for tbl in inTables:
+            arcpy.Delete_management(tbl)
     
+
        
 
