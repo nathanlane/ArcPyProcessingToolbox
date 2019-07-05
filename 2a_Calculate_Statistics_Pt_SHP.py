11096e80a49b3589a9ea69b1d86dd6e84b424361
Nathan Lane <nathaniel.x.lane@gmail.com>
2019-07-05 13:39:53 +1000

    Update of the 2a SHP file code for starting at folders
    Last version used to prepare whole list of files present in the input folder (whether lying at its root or in subdirectories) and then process each file, one by one from the list.
    
    If the folder contained 100 years of data, that means the list would contain 36,500 records corresponding to each file (with full path). Although, this much number of records shouldn't be a problem to handle theoretically, yet that might be the issue causing crashing of ArcGIS midway.
    
    The attached script avoids it by creating a list of 1 year only, processes each file of 1 year and then creates a new list for next year, processes it and so on.

--- 2a_Calculate_Statistics_Pt_SHP.py
+++ 2a_Calculate_Statistics_Pt_SHP.py
@@ -25,60 +25,63 @@ arcpy.env.overwriteOutput = True
 # Define variables related to tiff files in the input folder
 pattern = "prate_*.tif"                 # Pattern that will be used to find & prepare a list of raster files
 spattern = "prate_*.shp"                # Pattern that will be used to find & prepare a list of shapefiles
+dpattern = "prate.*"                    # Pattern that will be used to find & prepare a list of input folders
 lTIFs = []                              # Create a blank list that would be populated by input geotiff files later
-SHPs = []                               # Create a blank list that would be populated by input shapefiles later
+mydirs = []                             # Create a blank list that would be populated by input folders later
+
+# Create a list of child folders in the root folder containing GeoTiff Files
+for path, subdirs, files in os.walk(root):
+    for mydir in subdirs:
+        if fnmatch(mydir, dpattern):
+            mydirpath = os.path.join(path, mydir)
+            mydirs.append(mydirpath)
+# Delete empty shapefiles so that they may be generated anew
+    for name in files:        
+        if fnmatch(name, spattern):
+            SHP = os.path.join(path, name)               
+            if arcpy.management.GetCount(SHP)[0] == "0":
+                arcpy.AddMessage('Deleting empty shapefiles if any') 
+                arcpy.Delete_management(SHP)
 
 # Prepare a list of geotiff files matching the defined pattern from input folder
+for mydir in mydirs:
+    for path, subdirs, files in os.walk(mydir):
+        arcpy.AddMessage("\n" + 'Processing Folder ' + mydir + "\n")
+        for name in files:
+            if fnmatch(name, pattern):
+                TIF = os.path.join(path, name)
+                lTIFs.append(TIF)                 
+        # Loop through each raster file and calculate statistics
+        for tif in lTIFs:
+            tifpath, tifname = os.path.split(tif)       # Split filenames and paths
 
-for path, subdirs, files in os.walk(root):
-    for name in files:
-        if fnmatch(name, pattern):
-            TIF = os.path.join(path, name)
-            lTIFs.append(TIF)
-
-# Delete empty shapefiles so that they may be generated anew
-for path, subdirs, files in os.walk(root):
-    for name in files:
-        if fnmatch(name, spattern):
-            SHP = os.path.join(path, name)
-            SHPs.append(SHP)
+            ###################################################################
+            ## Definition of variables related to Point Shapefile Processing ##
+            ###################################################################
             
-if len(SHPs) > 0:
-    arcpy.AddMessage('Deleting empty shapefiles if any')
-    for shp in SHPs:
-        if arcpy.management.GetCount(shp)[0] == "0":
-            arcpy.Delete_management(shp)
+            ptout = tif.replace('.tif', '.shp')         # Full name & Path of temp output point shp
+           
+            ##############################################################
+            ## Start process to calulate statistics for point shapefile ##
+            ##############################################################
             
-# Loop through each raster file and calculate statistics
-for tif in lTIFs:
-    tifpath, tifname = os.path.split(tif)       # Split filenames and paths
+            # Delete temporary point shapefile if already exists
+            if not os.path.exists(ptout):
 
-    ###################################################################
-    ## Definition of variables related to Point Shapefile Processing ##
-    ###################################################################
-    
-    ptout = tif.replace('.tif', '.shp')         # Full name & Path of temp output point shp
-   
-    ##############################################################
-    ## Start process to calulate statistics for point shapefile ##
-    ##############################################################
-    
-    # Delete temporary point shapefile if already exists
-    if not os.path.exists(ptout):
+                arcpy.AddMessage('Processing ' + tifname)
+                try:
+                    arcpy.sa.ExtractValuesToPoints(ptshp, tif, ptout,
+                                      ptinterval, "VALUE_ONLY")
+                except:
+                    arcpy.AddMessage('Error in processing ' + tifname)
+            else:
+                arcpy.AddMessage('Skipping ' + tifname + " (Already Exists)")
 
-        arcpy.AddMessage('Processing ' + tifname)
-        try:
-            arcpy.sa.ExtractValuesToPoints(ptshp, tif, ptout,
-                              ptinterval, "VALUE_ONLY")
-        except:
-            arcpy.AddMessage('Error in processing ' + tifname)
-    else:
-        arcpy.AddMessage('Skipping ' + tifname + " (Already Exists)")
-
-    del ptout
-    del tif
+            del ptout
+            del tif
+        lTIFs = []
+            
         
-    
 
-       
+           
 
