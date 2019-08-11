fbbf2cd0e15f19dabce6a9ca7194a5144efa2bd2
nathanlane <nathaniel.x.lane@gmail.com>
2019-08-11 15:41:35 +1000

    Switching correcting NULL error
    Correcting the issue related to Extracting ExtractingRasterValues from extracting RasterValuesToTables

--- 2a_Calculate_Statistics_Pt_SHP.py
+++ 2a_Calculate_Statistics_Pt_SHP.py
@@ -10,12 +10,6 @@ root = arcpy.GetParameterAsText(0)      # Input Folder
 ptshp = arcpy.GetParameterAsText(1)     # Input Point Shapefile
 ptinter = arcpy.GetParameter(2)         # Interpolate values at point locations
 
-# Define local variables for calculating statistics
-if ptinter == True:
-    ptinterval = "INTERPOLATE"
-else:
-    ptinterval = "NONE"
-
 # Set Workspace to input folder
 arcpy.env.workspace = root
 
@@ -24,7 +18,6 @@ arcpy.env.overwriteOutput = True
 
 # Define variables related to tiff files in the input folder
 pattern = "prate_*.tif"                 # Pattern that will be used to find & prepare a list of raster files
-spattern = "prate_*.shp"                # Pattern that will be used to find & prepare a list of shapefiles
 dpattern = "prate.*"                    # Pattern that will be used to find & prepare a list of input folders
 mydirs = []                             # Create a blank list that would be populated by input folders later
 
@@ -34,13 +27,6 @@ for path, subdirs, files in os.walk(root):
         if fnmatch(mydir, dpattern):
             mydirpath = os.path.join(path, mydir)
             mydirs.append(mydirpath)
-# Delete empty shapefiles so that they may be generated anew
-    for name in files:        
-        if fnmatch(name, spattern):
-            SHP = os.path.join(path, name)               
-            if arcpy.management.GetCount(SHP)[0] == "0":
-                arcpy.AddMessage('Deleting empty shapefiles if any') 
-                arcpy.Delete_management(SHP)
 
 # define function to calculate stats from files in a given folder
 def calcstats(mydir):
@@ -54,31 +40,40 @@ def calcstats(mydir):
         # Loop through each raster file and calculate statistics
         for tif in lTIFs:
             tifpath, tifname = os.path.split(tif)       # Split filenames and paths
+            tbloutfield = tifname.split('.')[0]         # Get output csv filname without extension
+            tbloutfield = tbloutfield.replace('prate_', '')# Strip _pt from name
+            outcsv = tbloutfield + '.csv'
+            tbloutfield = "d"+tbloutfield[1:]           # Replace first char of date(year) by "d" to overrule a restriction
 
             ###################################################################
             ## Definition of variables related to Point Shapefile Processing ##
             ###################################################################
             
-            ptout = tif.replace('.tif', '.shp')         # Full name & Path of temp output point shp
+            ptout = tif.replace('.tif', '.dbf')         # Full name & Path of temp output point shp
            
             ##############################################################
             ## Start process to calulate statistics for point shapefile ##
             ##############################################################
             
-            # Delete temporary point shapefile if already exists
+            # Skip if output table already exists
             if not os.path.exists(ptout):
 
                 arcpy.AddMessage('Processing ' + tifname)
                 try:
-                    arcpy.sa.ExtractValuesToPoints(ptshp, tif, ptout,
-                                      ptinterval, "VALUE_ONLY")
+                    arcpy.ExtractValuesToTable_ga(ptshp, tif, ptout, "", "")                                
+                    fmap = '{} \\\"'.format(tbloutfield) + tbloutfield + "\\\" " + "true true false 19 Double 0 0 ,First,#," + ptout + ",Value,-1,-1;SrcID_Feat \\\"SrcID_Feat\\\" true true false 10 Long 0 10 ,First,#," + ptout + ",SrcID_Feat,-1,-1"
+                    arcpy.TableToTable_conversion(ptout, tifpath, outcsv, "", fmap, "")
+
+                    # Define local variables for calculating statistics
+                    if ptinter == True:
+                        arcpy.Delete_management(ptout)
+                        
                 except:
                     arcpy.AddMessage('Error in processing ' + tifname)
             else:
                 arcpy.AddMessage('Skipping ' + tifname + " (Already Exists)")
 
-            del ptout
-            del tif
+            del ptout, tif, tbloutfield, fmap, outcsv
         lTIFs = []
 
 # Prepare a list of geotiff files matching the defined pattern from input folder
@@ -86,7 +81,7 @@ if len(mydirs) > 0:
     for mydir in mydirs:
         calcstats(mydir)
 else:
-    caclstats(root)
+    calcstats(root)
             
         
 
